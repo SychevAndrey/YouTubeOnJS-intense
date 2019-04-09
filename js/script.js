98ffd854cf46199a4d30fcd2061a8cec337fa4be
Andrey S <nukemoore@gmail.com>
2019-04-09 11:40:15 +0300

    add night mode change

--- js/script.js
+++ js/script.js
@@ -33,3 +33,46 @@ function bindSlideToggle (trigger, boxBody, content, openClass) {
 }
 
 bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
+
+function switchMode() {
+    const white = '#fff';
+    const black = '#000';
+    let elems = ['.videos__item-descr', '.videos__item-views', '.header__item-descr'];
+
+    function setColor(elements, color) {
+        document.querySelectorAll(elements).forEach(element => {
+            element.style.color = color;
+        });
+    }
+
+    if (!night) {
+        night = true;
+        document.body.classList.add('night');
+
+        document.querySelectorAll('.hamburger > line').forEach(line => {
+            line.style.stroke = white;
+        });
+
+        elems.forEach(element => {
+            setColor(element, white);
+        });
+        
+
+    } else {
+        night = false;
+        document.body.classList.remove('night');
+        document.querySelectorAll('.hamburger > line').forEach(line => {
+            line.style.stroke = black;
+        });
+
+        elems.forEach(element => {
+            setColor(element, black);
+        });
+        
+    }
+}
+
+let night = false;
+switcher.addEventListener('change', () => {
+    switchMode();
+});
